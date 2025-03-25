3d1277055d91465a91643a76e27565d5529f5459
Saul Sosa <alu0101404141@ull.edu.es>
2025-03-25 19:17:38 +0000

    feat: integrate QueryClientProvider in App and HomeScreen for React Query support

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './HomeScreen.styles.ts';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HomeScreen } from './HomeScreen.tsx';
 
-const queryClient = new QueryClient()
+
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
       <HomeScreen />
-    </QueryClientProvider>
   );
 }
 
--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,10 +1,12 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './HomeScreen.styles.ts';
 import { Form, MainContainer } from './HomeScreen.styles.ts';
 import { AutoComplete } from 'primereact/autocomplete';
 import { Clues } from './components/clues.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { VIDEOGAMES } from './constants/videoGames.ts';
+import { videoGame } from './types/videoGame.ts';
 
 const queryClient = new QueryClient()
 
@@ -12,19 +14,20 @@ export const HomeScreen = () => {
     const [videoGameValue, setVideoGameValue] = useState<string>('');
     const [filteredVideogames, setFilteredVideogames] = useState<string[]>([]);
     const [image, setImage] = useState<number>(0);
+    const [solution, setSolution] = useState<videoGame | null>();
+    const options = VIDEOGAMES.map((videoGame) => videoGame.name);
 
-    const options = [
-        "Apex Legends",
-        "Call of Duty: Warzone",
-        "Counter",
-        "Strike: Global Offensive",
-        "Dota 2",
-        "Fortnite",
-        "Grand Theft Auto V",
-        "League of Legends",
-        "Minecraft",
-        "Overwatch",
-    ];
+
+    useEffect(() => {
+        const getRandomVideogame = (): videoGame => {
+            const randomIndex = Math.floor(Math.random() * VIDEOGAMES.length);
+            return VIDEOGAMES[randomIndex] as videoGame;
+        }
+        const randomVideogame = getRandomVideogame();
+        setSolution(randomVideogame);
+    }, []);
+
+
 
     const search = (event: { query: string }) => {
         const includes = new Set();
@@ -60,6 +63,8 @@ export const HomeScreen = () => {
         <QueryClientProvider client={queryClient}>
             <MainContainer>
                 <h1>Guess the Game</h1>
+                {solution && <>
+                    <img src={solution?.short_screenshots[1].image} alt="Game" />
                     <Clues />
                     <Form onSubmit={handleSubmit}>
                         <AutoComplete
@@ -72,7 +77,10 @@ export const HomeScreen = () => {
                         />
                         <button type="submit">Submit</button>
                     </Form>
-                <button onClick={() => setImage(image + 1)}>Next</button>
+                    <button onClick={() => setImage(image + 1)}>Next</button>
+                    {solution.name}
+                </>
+                }
             </MainContainer>
         </QueryClientProvider>
     );
